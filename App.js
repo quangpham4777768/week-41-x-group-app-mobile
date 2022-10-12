57d34f5bc575e2b233e2acd1dd37b30d4fbd3596
Nam Nguyen <meomaythongminh3@gmail.com>
2022-10-12 16:34:41 +0700

    Create branch nam

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* <Button onPress={() => navigation.navigate("MainPage")} title="Start"/> */}
       <StatusBar style="auto" />
     </View>
   );
